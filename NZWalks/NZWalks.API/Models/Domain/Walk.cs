c585b181ea7c837c10e150a408dfab668148ab20
arshadkhan-git <arshad.khan.ahamed@gmail.com>
2022-12-28 16:03:48 +0530

    End Of Section 2

---
+++ NZWalks/NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Length { get; set; }
+        public Guid RegionId { get; set; }
+        public Guid WalkDifficultyId { get; set; }
+
+        //Navigation Properties
+        public Region Region { get; set; }
+        public WalkDifficulty walkDifficulty { get; set; }
+    }
+}
